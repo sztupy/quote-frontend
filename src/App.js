6daff0895fcfd47c5da1cb810e0090b92b1c9f76
Zsolt Sz. Sztupák <mail@sztupy.hu>
2023-07-15 11:58:41 +0100

    Fix linting errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Quote from "./Quote";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [quote, setQuote] = useState({});
--- src/Quote.js
+++ src/Quote.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 
-function Quote({quote}) {
+function Quote({ quote }) {
     return <div>
         <p>{quote.quote}</p>
         <p>{quote.author}</p>
-    </div>
+    </div>;
 }
 
 export default Quote;
