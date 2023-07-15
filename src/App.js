afc0fe8f90a86121ee79eb0d562709a1a0a9d0c6
Zsolt Sz. Sztupák <mail@sztupy.hu>
2023-07-15 12:03:28 +0100

    Fixed ticket #2

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [quote, setQuote] = useState({});
+  const [quoteCount, setQuoteCoun] = useState(0);
 
   useEffect(() => {
     fetch("https://likeable-standing-beryl.glitch.me/quotes/random")
       .then((res) => res.json())
       .then((res) => setQuote(res));
-  },[]);
+  },[ quoteCount ]);
 
   return (
     <div className="App">
       <header className="App-header">
         <Quote quote={quote}></Quote>
+        <button onClick={() => setQuoteCoun(quoteCount + 1)}>Get new quote</button>
       </header>
     </div>
   );
